93dd6cb16f0aac7893f489c0352e6cafa7eb76c4
viviane-vieira <112434357+viviane-vieira@users.noreply.github.com>
2024-06-13 20:10:59 -0300

    inicializando o projeto com  Orientação a objeto

---
+++ index.js
@@ -0,0 +1,16 @@
+class Produto {
+    constructor() {
+        this.id = 0;
+        this.nameProduct = " ";
+        this.valor = 0
+
+    }
+    adicionar(){
+        alert("vamos adicionar produtos");
+    }
+    deletar(){
+        alert("produto exluido com sucesso")
+    }
+}
+
+const produto = new Produto();
