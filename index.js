8fa9430507e5b01f93157527997c8804b15d75d1
viviane-vieira <112434357+viviane-vieira@users.noreply.github.com>
2024-06-19 21:08:01 -0300

    criando nossa tabela dinamica cominteração do javascript

--- index.js
+++ index.js
@@ -1,15 +1,61 @@
 class Produto {
     constructor() {
-        this.id = 0;
-        this.nameProduct = " ";
-        this.valor = 0
+        this.id = 1;
+        this.arrayProdutos = [];
 
     }
-    adicionar(){
-        alert("vamos adicionar produtos");
+    salvar() {
+       let produto = this.lerDados();
+
+       if(this.validaCampo(produto)) {
+          this.adicionar(produto);
+       }
+       console.log(this.arrayProdutos)
     }
-    deletar(){
-        alert("produto exluido com sucesso")
+    adicionar() {
+       this.arrayProdutos.push(produto);
+       this.id++;
+    }
+    lerDados() {
+        let produto = {}
+        
+       produto.id = this.id;
+       produto.nomeProduto = document.getElementById('produto').value;
+       produto.cor = document.getElementById('cor').value;
+       produto.preco = document.getElementById('preco').value;
+       produto.tamanho = document.getElementById('tamanho').value;
+      
+
+
+        return produto;
+
+    }
+    validaCampo(produto) {
+        let msg = ''
+        if(produto.nomeProduto == '') {
+            msg += '- Informe o Nome do Produto \n';
+        }
+        if(produto.preco == '') {
+            msg += '- Informe o Preço do Produto \n';
+        }
+        if(produto.cor == '') {
+            msg += '- Informe a Cor do Produto \n';
+        }
+        if(produto.tamanho == '') {
+            msg += '- Informe o tamanho do Produto \n';
+        }
+
+        if (msg != '') {
+            alert(msg);
+            return false
+        }
+        return true
+
+    }
+
+    
+    cancelar(){
+       
     }
 }
 
